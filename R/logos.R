27e5fe19dc6964ccbdd921dfd8f13242138f8945
prdm0 <pedro.rafael.marinho@gmail.com>
2022-04-14 10:13:24 -0300

    Add mapa iterativo na aba principal.

--- R/logos.R
+++ R/logos.R
@@ -1,3 +1,7 @@
 #logo <- magick::image_read("img/logo.png")
 #logo <- magick::image_resize(logo,"48x48!")
 #magick::image_write(logo, path = "img/logo_menor.png", format = "png")
+
+logo_texto <- magick::image_read("img/logo.png")
+logo_texto <- magick::image_resize(logo_texto,"120x72!")
+magick::image_write(logo_texto, path = "img/logo_menor.png", format = "png")
