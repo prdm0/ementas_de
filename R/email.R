0df26d9af1d9004222a532fc1e798b91835f9611
prdm0 <pedro.rafael.marinho@gmail.com>
2022-04-18 16:54:52 -0300

    Add informações de avisos ao posicionar o mouse em cima da chave de envio de email.

--- R/email.R
+++ R/email.R
@@ -1,18 +1,60 @@
 library(gmailr)
 
-# Autentique aqui
+gmailr::gm_auth_configure(
 
-send_email <- function(nome, matricula, data, hora, local) {
+)
+gmailr::gm_auth(
 
+)
+
+send_email <- function(nome = NULL, matricula = NULL, data = NULL, hora = NULL, local = NULL, tipo = "COMP") {
+  
+  email <-
+  switch(
+    tipo,
+    'COMP' =  {
+        list(
+          subject = paste0("COMPLEMENTAÇÃO: ", nome,  " (matrícula Nº ", matricula, ")"),
+          body = paste0("A prova de complementação do discente ", nome, " (matrícula Nº ",
+                matricula, ") será realizada em ", data, ", às ", hora, ", na(o) ", local, "."),
+          pdf = "parecer/enviar_email/despacho_complementacao.pdf"
+        )
+    }, 
+    
+    'DE' = {
+      list(
+        subject = paste0("DEFERIDO: ", nome,  " (matrícula Nº ", matricula, ")"),
+        body = paste0("A solicitação do discente ", nome, " (matrícula Nº ",
+                       matricula, ") foi DEFERIDA em ", format(Sys.Date(), "%d/%m/%Y")),
+        pdf = "parecer/enviar_email/despacho_deferido.pdf"
+      )
+    },
+    
+    'IND' = {
+      list(
+        subject = paste0("INDEFERIDO: ", nome,  " (matrícula Nº ", matricula, ")"),
+        body = paste0("A solicitação do discente ", nome, " (matrícula Nº ",
+                      matricula, ") foi INDEFERIDA em ", format(Sys.Date(), "%d/%m/%Y"), "."),
+        pdf = "parecer/enviar_email/despacho_indeferido.pdf"
+      )
+    },
+    
+    'DESP' = {
+      list(
+        subject = paste0("DESPACHO"),
+        body = paste0("Despacho redigido em ", format(Sys.Date(), "%d/%m/%Y"), "."),
+        pdf = "parecer/enviar_email/despacho.pdf"
+      )
+    }
+  )
+  
   test_email <-
     gm_mime() |>
     gm_to("comissao.dispensa.estatistica@gmail.com") |>
     gm_from("comissao.dispensa.estatistica@gmail.com") |>
-    gm_subject(paste0("Complementação: ", nome,  " matrícula Nº ", matricula)) |>
-    gm_text_body(
-        paste0("Prova de complementação do discente ", nome, " matrícula Nº ", matricula, " para ser realizada em ", data, " as ",  "hora ", "na(o) ", local,".")
-    ) |>
-    gm_attach_file("parecer/enviar_email/despacho_complementacao.pdf")
+    gm_subject(email$subject) |>
+    gm_text_body(email$body) |>
+    gm_attach_file(email$pdf)
   
   # Verify it looks correct
   gm_create_draft(test_email, user_id = "me")
